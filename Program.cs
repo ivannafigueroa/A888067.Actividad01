cfe1a2adf09473aaab4e0bf9024f1745c3e61ff9
ivannafigueroa <79493317+ivannafigueroa@users.noreply.github.com>
2021-03-17 15:31:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A888067.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
